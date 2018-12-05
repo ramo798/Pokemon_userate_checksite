c10e0b5f96c17f0e942db9f04fc4ccb04d76d63b
ramo <knht31i@gmail.com>
2018-12-05 23:36:59 +0900

    URL通したINDEX

--- myDjango/urls.py
+++ myDjango/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+from django.conf.urls import include, url
+from main  import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
